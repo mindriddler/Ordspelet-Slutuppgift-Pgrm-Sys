b063461d7c0fbe15f4397d99c41577a709d85204
Fredrik <raekon92@gmail.com>
2022-09-27 11:56:54 +0200

    small changes

--- _functions.py
+++ _functions.py
@@ -1,3 +1,7 @@
+import random
+
+
+
 def splash_screen():
     print("""
 -----------------------------
@@ -11,7 +15,7 @@ Spelregler är som följer!
 6. Ordet får inte ha upprepade bokstäver, så som "hemma".""")
     accept = input("\nAccepterar du spelreglerna? ").lower()
     if accept == "ja":
-        print(main_menu())
+        main_menu()
         menu_choice()
     elif accept == "nej":
         print("Då får du inte lov att vara med och spela.\nAvslutar spelet.")
@@ -23,20 +27,32 @@ Spelregler är som följer!
 
 def main_menu():   
     
-    return """
+    print("""
 ----------MAIN MENU----------
 1. Spela som gissare    
 2. Spela som tänkare
 -----------------------------
-9. Avsluta"""
+9. Avsluta""")
+
+def get_word():
+    try:
+        file = open("words.txt", "r", encoding="utf-8")
+        words = file.read()
+        listOfWords = words.split("\n")
+        randWord = random.choice(listOfWords)
+        return randWord
+    except FileNotFoundError:
+        print("Filen 'words.txt' måste finnas i din nuvarande mapp för att spelet ska köra.\nVänligen lägg till filen och försök igen.")
+        exit()
+
 
 def menu_choice():
     try:
         choice = int(input("Välj ett alternativ: "))
         if choice == 1:
             user_guess()
-        elif choice == 2:
-            user_think() # Creating at a later stage
+        # elif choice == 2:
+            # user_think() Creating at a later stage
     except ValueError:
         print("Vänligen ange ett giltligt alternativ.")
         menu_choice()
@@ -49,22 +65,12 @@ def user_guess():
     if guess == get_word().lower():
         print("Du gissade rätt!")
 
-    
-    
 
-def get_word():
-    try:
-        file = open("words.txt", "r", encoding="utf-8")
-        words = file.read()
-        listOfWords = words.split("\n")
-        randWord = "Polis" #random.choice(listOfWords)
-        return randWord
-    except FileNotFoundError:
-        print("Filen måste finnas i din nuvarande mapp för att programmet ska köra.")
-        exit()
+
         
         
-        
+
+get_word()        
 splash_screen()
-get_word
+
 
